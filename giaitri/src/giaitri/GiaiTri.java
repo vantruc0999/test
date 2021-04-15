5866a62ef4d4d91ab812a2696bedb2c07585de72
vantruc0999 <37946728+vantruc0999@users.noreply.github.com>
2021-04-15 09:53:15 +0700

    Update GiaiTri.java
    add comment in line 64

--- giaitri/src/giaitri/GiaiTri.java
+++ giaitri/src/giaitri/GiaiTri.java
@@ -60,8 +60,8 @@ public class GiaiTri {
 			 a[i] = a[a.length-i-1];
 			 a[a.length-i-1] = temp;
 		 }
-	 }
-	 
+	 } 
+	//ham main
 	public static void main(String[] args) {
 			
 			int a[] = {6,5,4,8};
